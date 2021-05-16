57741fa44bc094e430d4deda354dd8a3349b9d10
Emil Ahlbäck <emil@tentium.se>
2021-05-16 13:10:01 +0200

    setup aaaa

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -3,12 +3,9 @@ curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/
 chmod +x ~/.local/bin/rust-analyzer
 echo 'Ensure $HOME/.local/bin is in the $PATH variable'
 
-npm i -g pyright
-npm i -g prettier eslint typescript typescript-language-server
-
-npm install -g vim-language-server
-npm i -g sql-language-server json-language-server vscode-css-languageserver-bin
-npm i -g vscode-html-languageserver-bin bash-language-server
+npm i -g pyright prettier eslint typescript typescript-language-server sql-language-server \
+  json-language-server vscode-css-languageserver-bin vscode-html-languageserver-bin \
+  bash-language-server vim-language-server
 
 go get github.com/mattn/efm-langserver
 
